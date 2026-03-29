dc9575c27b25298b361e488fe5b89b170fd20df7
Debarun <paldebarun27@gmail.com>
2026-03-29 21:29:44 +0530

    modified the config and .gitignore file

--- configurations/config.py
+++ configurations/config.py
@@ -1,1 +1,13 @@
-UPLOAD_DIR = "uploads"
+import os 
+
+
+UPLOAD_DIR = os.environ.get("UPLOAD_DIR","uploads")
+POSTGRES_USER=os.environ.get("POSTGRES_USER","admin")
+POSTGRES_PASSWORD=os.environ.get("POSTGRES_PASSWORD","admin")
+POSTGRESS_PORT=os.environ.get("POSTGRESS_PORT",5432)
+POSTGRES_DB=os.environ.get("POSTGRES_DB","docling_db")
+POSTGRES_HOST=os.environ.get("POSTGRES_HOST","localhost")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRESS_PORT}/{POSTGRES_DB}"
+
+PORT=os.environ.get("SERVER_PORT",8082)
